a1809896c723a3e980f5eec18ecbd3e6c7c1715d
Michael Oberst <michael.k.oberst@gmail.com>
2017-06-10 22:40:28 -0700

    Create another dummy test and use against the actual module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,18 @@
 import pytest
 import numpy as np
+import os
+import sys
+sys.path.append('{}/repos/mypkg/src'.format(os.getenv('HOME')))
 
 
 @pytest.fixture(scope="module")
 def vec1():
     vec = np.array([0, 1, 2, 3, 4])
     yield vec  # provide the fixture value
-    print("teardown vec1")
+    print("Teardown goes here")
+
+@pytest.fixture(scope="module")
+def vec2():
+    vec = np.array([0, 2, 4, 6, 8])
+    yield vec  # provide the fixture value
+    print("Teardown goes here")
--- tests/test_avg.py
+++ tests/test_avg.py
@@ -1,4 +1,10 @@
-def test_avg(vec1):
-    # TODO: Import the "avg" function to test
+from mypkg import myfunc as f
+
+
+def test_avg1(vec1):
     assert len(vec1) == 5
-    assert 0
+    assert f.avg(vec1) == 2
+
+def test_avg2(vec2):
+    assert len(vec2) == 5
+    assert f.avg(vec2) == 4
